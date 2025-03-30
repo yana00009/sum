db49d34d0d48b92d6721fcf5cca07494f3191137
yana00009 <151913745+yana00009@users.noreply.github.com>
2025-03-30 17:05:38 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ class Program
         int a = int.Parse(Console.ReadLine());
         int b = int.Parse(Console.ReadLine());
         int sum = a + b;
+        sd
         Console.WriteLine("The sum of a and b is : " + sum);
         Console.ReadKey();
     }
