c56f332edae53bf273c95ac2a705c666f9647ac5
yana00009 <151913745+yana00009@users.noreply.github.com>
2025-03-30 16:54:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace sum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int sum = a + b;
+        Console.WriteLine("The sum of a and b is : " + sum);
+        Console.ReadKey();
+    }
+}
